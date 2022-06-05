024da6ca337ea31bbbc2345aebf24b4ad1fd42e4
Nikita Volskii <nikitoriy@gmail.com>
2022-06-05 23:19:35 +0300

    Now the graph to be shown is updated on ticker choice from the dropdown

--- vals_presenter.py
+++ vals_presenter.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, callback, Input, Output
 import plotly.express as px
 import psycopg2
 from vals_generator import PG_CONF
@@ -9,34 +9,52 @@ SELECT_VALUES_QUERY='''select ticker_value,processed_dttm from tickers_values wh
 '''
 
 
-def generate_graph(ticker_name):
+# def generate_graph(ticker_name):
+#     with psycopg2.connect(**PG_CONF) as conn:
+#         cur = conn.cursor()
+#         cur.execute(SELECT_VALUES_QUERY, (ticker_name,))
+#         res = cur.fetchall()
+#     df = pd.DataFrame({
+#         "Time": [t for (_,t) in res],
+#         "Value": [v for (v,_) in res]
+#     })
+#     fig = px.line(df, x='Time', y='Value')    
+#     return fig
+
+
+
+def generate_app():
+    app = Dash(__name__)
+    tickers = [f'ticker_{i:02}' for i in range(99)]
+    app.layout = html.Div(children=[
+        html.H1('Showing_graphs'),
+        html.Label('Chose a ticker'),
+
+        dcc.Dropdown(
+            tickers,
+            'choose_ticker',
+            id='choose_ticker'
+        ),
+        dcc.Graph(
+            id='ticker_graph'),
+    ])
+    return app
+
+@callback(
+    Output('ticker_graph', 'figure'),
+    Input('choose_ticker', 'value')
+)
+def update_graph(ticker):
     with psycopg2.connect(**PG_CONF) as conn:
         cur = conn.cursor()
-        cur.execute(SELECT_VALUES_QUERY, (ticker_name,))
+        cur.execute(SELECT_VALUES_QUERY, (ticker,))
         res = cur.fetchall()
-    # return res
     df = pd.DataFrame({
         "Time": [t for (_,t) in res],
         "Value": [v for (v,_) in res]
     })
     fig = px.line(df, x='Time', y='Value')    
     return fig
-
-
-
-def generate_app():
-    app = Dash(__name__)
-    tick = 'ticker_03'
-    fig = generate_graph(tick)
-    tickers = [f'ticker_{i:02}' for i in range(99)]
-    app.layout = html.Div(children=[
-        html.H1('Showing_graphs'),
-        html.Label('Chose a ticker'),
-
-        dcc.Dropdown(tickers),
-        dcc.Graph(id=tick,figure=generate_graph(tick)),
-    ])
-    return app
 
 if __name__ == '__main__':
     app = generate_app()
